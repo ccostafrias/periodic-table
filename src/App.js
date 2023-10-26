474aca5df6c899e15def74edc1f48249fbdc9142
Caique Costa <ccostafrias@gmail.com>
2023-10-26 20:20:37 -0300

    update periodic table

--- src/App.js
+++ src/App.js
@@ -11,16 +11,46 @@ export default function App() {
 
     }, [])
 
-    if (atomic) {
-        console.log(atomic.map(a => `${a.xpos} e ${a.ypos}`))
-    }
+    const atomicAlements = atomic?.map(a => {
+        const column = a.xpos
+        const row = a.ypos
+        const { symbol, category, name, number } = a
+        const categoryAbbr = category.split(' ').map(c => c.charAt(0)).join('')
+        console.log(categoryAbbr)
+        const style = {
+            gridColumn: `${column} / span 1`,
+            gridRow: `${row} / span 1`
+        }
+
+        return (
+            <div 
+                style={style}
+                className={`atom ${categoryAbbr}`}
+            >
+                <h2 className="atomic-symbol">{symbol}</h2>
+                <span className="atomic-name">{name}</span>
+                <span className="atomic-number">{number}</span>
+            </div>
+        )
+    })
 
 
     return (
         <>
-            <main></main>
-            <div className="periodic-table"></div>
-           <h1>Hello, World!</h1>
+            <header className="header-periodic">
+                <h1>Periodic Table</h1>
+                <nav></nav>
+            </header>
+            <main className="main-periodic">
+                {atomic ? (
+                    <div className="periodic-table">
+                        {atomicAlements}
+                    </div>
+                ) : (
+                    <h1>Loading...</h1>
+                )}
+            </main>
+           {/* <h1>Hello, World!</h1> */}
         </>
        
     )
